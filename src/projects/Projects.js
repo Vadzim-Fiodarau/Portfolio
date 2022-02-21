4a3ead98aa5e533b754c0f7dc6775793b6cf83a6
Vadzim-Fiodarau <fioadarau.v@gmail.com>
2022-02-21 23:37:11 +0300

    add styles for project

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,10 +1,19 @@
 import styles from './Projects.module.css'
 import styleContainer from '../common/styles/Container.module.css'
-import {Project} from "./project/Projects";
+import {Project} from "./project/Project";
 import {Title} from "../common/components/title/Title";
+import todoImage from './../assets/image/todo.jpg'
+import socialImage from './../assets/image/social.jpg'
 
 
 export const Projects = () => {
+    const social = {
+        backgroundImage: `url(${socialImage})`
+    }
+    const todo = {
+        backgroundImage: `url(${todoImage})`
+    }
+
     return (
         <div className={styles.projectsBlock}>
             <div className={`${styleContainer.container} ${styles.projectsContainer}`}>
@@ -14,12 +23,12 @@ export const Projects = () => {
                     <Project title={'Todolist'}
                              description={'My first project in IT-INCUBATOR'}
                              link={'https://vadzim-fiodarau.github.io/Todolist'}
-                             // image={'https://wallpaperaccess.com/full/2005056.png'}
+                             style={social}
                     />
                     <Project title={'Social Network'}
                              description={'My second project in IT-INCUBATOR'}
                              link={'https://Vadzim-Fiodarau.github.io/SocialNetwork'}
-                             // image={'https://wallpaperaccess.com/full/2005056.png'}
+                             style={todo}
                     />
 
                 </div>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -4,15 +4,16 @@ import styles from './Project.module.css'
 export const Project = (props) => {
     return (
         <div className={styles.project}>
-            <div className={styles.imageContainer}>
+            <div className={styles.imageContainer} style={props.style}>
                 <a href={props.link}
                    className={styles.projectButton}
                 >Open project</a>
             </div>
-            <h3>{props.title}</h3>
-            <div className={styles.description}>
-                {props.description}
+            <div className={styles.projectInfo}>
+                <h3 className={styles.projectTitle}>{props.title}</h3>
+                <span className={styles.description}>{props.description}</span>
             </div>
+
         </div>
     )
 }
